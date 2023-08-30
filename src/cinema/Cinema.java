24b9c2b6c017dc08351550040a0142dc04943740
yrodr <y.rodr1gues0811@outlook.com>
2023-08-30 17:44:46 -0300

    iniciando classe cinema e classe de testes para cinema.

--- src/cinema/Cinema.java
+++ src/cinema/Cinema.java
@@ -1,4 +1,40 @@
 package cinema;
 
+import ui.Mapa;
+import ui.Menu;
+
+import java.util.Scanner;
+
 public class Cinema {
+    Mapa mapa;
+    Menu menu;
+    Opcoes opcoes;
+    Scanner input;
+    boolean operando;
+
+    public Cinema(){
+        this.mapa = new Mapa();
+        this.menu = new Menu();
+        this.opcoes = new Opcoes();
+        input = new Scanner(System.in);
+        operando = true;
+
+        while(operando){
+            menu.imprimeMenu();
+            int escolha = input.nextInt();
+            switch (escolha){
+                case 1:;
+                case 2:;
+                case 3:;
+                case 4:;
+                case 5:;
+                    System.out.println("Operação cancelada.");
+                    operando = false;
+                    continue;
+                default:
+                    System.out.println("Escolha uma opção válida.");
+
+            }
+        }
+    }
 }
---
+++ src/testes/CinemaTeste.java
@@ -0,0 +1,9 @@
+package testes;
+
+import cinema.Cinema;
+
+public class CinemaTeste {
+    public static void main(String[] args) {
+        Cinema cinema = new Cinema();
+    }
+}
