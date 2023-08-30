d9878d7b388351a59fc5c3f2f4042881ca14383a
yrodr <y.rodr1gues0811@outlook.com>
2023-08-30 17:20:45 -0300

    metodo de imprimir na classe Mapa e classe MapaTeste

---
+++ src/testes/MapaTeste.java
@@ -0,0 +1,11 @@
+package testes;
+
+import ui.Mapa;
+
+public class MapaTeste {
+    public static void main(String[] args) {
+        Mapa mapa = new Mapa();
+
+        mapa.imprimeAssentos();
+    }
+}
--- src/ui/Mapa.java
+++ src/ui/Mapa.java
@@ -1,4 +1,47 @@
 package ui;
 
 public class Mapa {
+    private char[][] assentos;
+    private int quantLivres;
+    private int quantOcupados;
+
+    public Mapa() {
+        this.assentos = new char[12][14];
+
+        for (int i = 0; i < this.assentos.length; i++) {
+            for (int j = 0; j < this.assentos[i].length; j++) {
+                assentos[i][j] = 'O';
+            }
+        }
+
+        this.quantLivres = 168;
+        this.quantOcupados = 0;
+    }
+
+    public void imprimeAssentos() {
+        char[] letras = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L'};
+        System.out.println("    1 2 3 4 5 6 7    8 9 10 11 12 13 14");
+
+        for (int i = 0; i < letras.length; i++) {
+            System.out.print(letras[i] + " - ");
+            imprime(i);
+            System.out.println();
+        }
+    }
+
+    public void imprime(int linha) {
+        for (int j = 0; j < assentos[linha].length / 2; j++) {
+            System.out.print(assentos[linha][j] + " ");
+        }
+        System.out.print("|| ");
+
+        for (int j = assentos[linha].length / 2; j < assentos[linha].length; j++) {
+            if (j >= 9) {
+                System.out.print(assentos[linha][j] + "  ");
+            } else {
+                System.out.print(assentos[linha][j] + " ");
+            }
+        }
+
+    }
 }
