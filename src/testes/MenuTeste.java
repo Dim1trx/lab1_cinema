5d78a9a879629e78b0dd1219cec828044a04c53c
yrodr <y.rodr1gues0811@outlook.com>
2023-08-30 17:44:16 -0300

    metodo de imprimir menu e classe para testes de menu.

---
+++ src/testes/MenuTeste.java
@@ -0,0 +1,10 @@
+package testes;
+
+import ui.Menu;
+
+public class MenuTeste {
+    public static void main(String[] args) {
+        Menu menu = new Menu();
+        menu.imprimeMenu();
+    }
+}
--- src/ui/Menu.java
+++ src/ui/Menu.java
@@ -1,4 +1,14 @@
 package ui;
 
 public class Menu {
+    public void imprimeMenu(){
+        System.out.println("------------------------------------------------------");
+        System.out.println("Bem vindo! Selecione a operação desejada: ");
+        System.out.println("1. Comprar ingresso.");
+        System.out.println("2. Cancelar ingresso.");
+        System.out.println("3. Mostrar os lugares disponiveis. ");
+        System.out.println("4. Verificar quantidade de total de pessoas na sessão.");
+        System.out.println("5. Sair.");
+        System.out.println("------------------------------------------------------");
+    }
 }
